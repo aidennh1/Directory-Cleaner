7fc03ffebbe3bfa7a832d540f5b0f67777b57373
aidennh1 <aiden.nhowgate@gmail.com>
2026-05-27 22:04:30 -0400

    Update file_handler.py

--- src/file_handler.py
+++ src/file_handler.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+import os
 
 def get_file_types(dir_in_str: str) -> set[str]:
     return {
@@ -6,3 +7,11 @@ def get_file_types(dir_in_str: str) -> set[str]:
         for file in Path(dir_in_str).iterdir()
         if file.is_file()
     }
+
+def create_type_folder(type_set: set, directory: str):
+    for type in type_set:
+        try:
+            type_str = str(type).strip('.')
+            os.mkdir(f"{directory}\{type_str}")
+        except FileExistsError:
+            print(f"Folder {type_str} already exists")
